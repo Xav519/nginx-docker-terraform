072896297c90fd000c1434217f8ec921a0a60d5d
Xav519 <dupuis_x@yahoo.com>
2025-11-28 01:06:55 -0500

    Updated main.tf and variables.tf structure

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,5 +1,3 @@
-// main.tf
-
 terraform {
   required_providers {
     aws = {
@@ -14,24 +12,7 @@ terraform {
 }
 
 provider "aws" {
-  region = var.aws_region
-}
-
-############################
-# Build & Push Docker Image (local)
-############################
-resource "null_resource" "docker_build_push" {
-  provisioner "local-exec" {
-    command = <<EOT
-      echo "Building Docker image..."
-      docker build -t ${var.docker_username}/${var.docker_image}:latest ..
-      echo "Logging into Docker Hub..."
-      docker login -u ${var.docker_username}
-      echo "Pushing Docker image..."
-      docker push ${var.docker_username}/${var.docker_image}:latest
-    EOT
-    working_dir = "${path.module}/.."
-  }
+  region = var.region
 }
 
 ############################
@@ -40,22 +21,25 @@ resource "null_resource" "docker_build_push" {
 resource "aws_elastic_beanstalk_application" "nginx_app" {
   name        = var.application_name
   description = "Nginx Docker App deployed via Terraform"
-  depends_on  = [null_resource.docker_build_push]
 }
 
 ############################
-# IAM Role & Instance Profile (for EC2 instances)
+# IAM Role & Instance Profile
 ############################
 resource "aws_iam_role" "eb_instance_role" {
-  name = "${var.application_name}-instance-role"
+  name = "nginx-eb-instance-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = { Service = "ec2.amazonaws.com" }
-      Action = "sts:AssumeRole"
-    }]
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
   })
 }
 
@@ -65,43 +49,8 @@ resource "aws_iam_role_policy_attachment" "eb_instance_role_policy" {
 }
 
 resource "aws_iam_instance_profile" "eb_instance_profile" {
-  name = var.instance_profile_name
+  name = "nginx-eb-instance-profile"
   role = aws_iam_role.eb_instance_role.name
-}
-
-############################
-# S3 bucket for EB app versions
-############################
-resource "random_id" "suffix" {
-  byte_length = 4
-}
-
-resource "aws_s3_bucket" "eb_bucket" {
-  bucket = "${var.s3_bucket_prefix}-${random_id.suffix.hex}"
-  # keep defaults; don't set ACL resource (Bucket owner enforced in many accounts)
-}
-
-############################
-# Upload zipped Dockerrun (nginx-app.zip) to S3
-# Make sure nginx-app.zip is in the same folder as this main.tf
-############################
-resource "aws_s3_object" "app_version" {
-  bucket = aws_s3_bucket.eb_bucket.id
-  key    = "nginx-app.zip"
-  source = "nginx-app.zip"
-  acl    = "private"
-}
-
-############################
-# EB Application Version (points to S3 object)
-############################
-resource "aws_elastic_beanstalk_application_version" "app_version" {
-  name        = "v1"
-  application = aws_elastic_beanstalk_application.nginx_app.name
-  bucket      = aws_s3_bucket.eb_bucket.id
-  key         = aws_s3_object.app_version.key
-
-  depends_on = [null_resource.docker_build_push]
 }
 
 ############################
@@ -111,20 +60,26 @@ resource "aws_elastic_beanstalk_environment" "nginx_env" {
   name                = var.environment_name
   application         = aws_elastic_beanstalk_application.nginx_app.name
   solution_stack_name = "64bit Amazon Linux 2 v4.4.0 running Docker"
-  version_label       = aws_elastic_beanstalk_application_version.app_version.name
 
-  # Attach IAM instance profile so EC2 instances can run properly
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "IamInstanceProfile"
     value     = aws_iam_instance_profile.eb_instance_profile.name
   }
 
+  # Tell EB which Docker image to run
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "DOCKER_IMAGE"
+    value     = "${var.docker_username}/${var.docker_image}:latest"
+  }
+
+  # Optional: set other environment variables
   setting {
     namespace = "aws:elasticbeanstalk:application:environment"
     name      = "ENV"
     value     = "production"
   }
 
-  depends_on = [aws_elastic_beanstalk_application_version.app_version]
+  depends_on = [aws_elastic_beanstalk_application.nginx_app]
 }
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,43 +1,24 @@
-// variables.tf
-
-variable "aws_region" {
-  description = "AWS region to deploy into"
-  type        = string
-  default     = "us-east-1"
-}
-
 variable "docker_username" {
-  description = "Docker Hub username"
-  type        = string
-  default     = "xav519"
+  type    = string
+  default = "xav519"
 }
 
 variable "docker_image" {
-  description = "Docker image repository name (no tag)"
-  type        = string
-  default     = "nginx-docker-terraform"
-}
-
-variable "application_name" {
-  description = "Elastic Beanstalk application name"
-  type        = string
-  default     = "nginx-app"
+  type    = string
+  default = "nginx-docker-terraform"
 }
 
 variable "environment_name" {
-  description = "Elastic Beanstalk environment name"
-  type        = string
-  default     = "nginx-env"
+  type    = string
+  default = "nginx-env"
 }
 
-variable "s3_bucket_prefix" {
-  description = "Prefix for S3 bucket name (random suffix appended)"
-  type        = string
-  default     = "nginx-docker-terraform-bucket"
+variable "application_name" {
+  type    = string
+  default = "nginx-app"
 }
 
-variable "instance_profile_name" {
-  description = "IAM instance profile name for EB EC2 instances"
-  type        = string
-  default     = "nginx-eb-instance-profile"
+variable "region" {
+  type    = string
+  default = "us-east-1"
 }
